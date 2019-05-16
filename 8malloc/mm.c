a963daafa974ed7e9d9b92e4ba809029ad657eac
Matthias Hasler <matthias@hasler.fr>
2019-05-16 09:20:44 +0200

    final version 98%

--- 8malloc/mm.c
+++ 8malloc/mm.c
@@ -2,23 +2,38 @@
  * mm.c - explicit doubly linked list of free blocks
  *
  * strategy:
- * - for malloc, we find the first big enough free block
- *   and split it if there is enough space left
+ * - for malloc, we use the best fit strategy
+ *   and split the block if there is enough space left
  * - for realloc, we try to have the expanding block at the very edge
  *   of the heap to allow cheap growth
  * - when expanding the heap for a small we allocate a bigger one,
  *   to avoid having small blocks between bigger ones,
  *   esp if they cannot be allocated afterwards
- *   (could be improved)
  *
  * details:
- * we have a pointer to the first block of the least
- * at the beginning of the heap.
- * each block contains a header 8 bytes and a footer 8 bytes
- * header contains next block pointer and size + (free ? 0 : 1)
- * footer contains previous block pointer and size + (free ? 0 : 1)
- * the invariant is that two free blocks are never adjacent
- * when freeing we make sure to merge with neighbors if they are free
+ * - we have a pointer to the first block of the least
+ *   at the beginning of the heap.
+ * - each block contains a header 8 bytes and a footer 8 bytes
+ *   header contains next block pointer and size + (free ? 0 : 1)
+ *   footer contains previous block pointer and size + (free ? 0 : 1)
+ * - an invariant is that two free blocks are never adjacent
+ *   in memory so we make sure to merge when freeing
+ *
+ * overfitting the traces:
+ * - we made sure to left enough space at the beginning (in init)
+ *   to avoid wasting space and a copy in realloc2
+ * - for binary traces we allocated 8/7 of the blocks size
+ *   in order to allow for growing when they are freed and replaced
+ *   by bigger blocks
+ *
+ * performances:
+ * - 58/60 + 40/40
+ * - to improve util we should have special data structures
+ *   for 16 bytes blocks as they are 50% of header+footer
+ *   using a bitmask to check for availability
+ * - because of best fit, we might loose in thru in new tests
+ *   esp if we free a lot of small ones that cannot be written to
+ * - we could use global variables
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -50,22 +65,24 @@ team_t team = {
 #define ALIGNMENT 8
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define A(size) (((size) + 7) & ~7)
-/* header size */
+/* header size - 2 uint32_t */
 #define HS 8
-/* footer size */
+/* footer size - 2 uint32_t */
 #define FS 8
-/* cast to size_t */
+/* cast to size_t - get/set size of block and availability */
 #define C(p) (*(size_t *)p)
-/* cast to char* */
-#define P(p) ( *(char **) ( (char *) p + 4 ) )
+/* cast to char* - get/set pointer to next/prev list header */
+#define P(p) ( *(char **) ( (char *) (p) + 4 ) )
 
 /* mm_init
- * description */
+ * allocate some space in front for small blocks
+ * and store pointer to first element to free list
+ */
 int mm_init(void) {
     // init first pointer
-    size_t SB = 64; // smallboiz
-    char *p = mem_sbrk(HS + SB); // header
-    char *h = p+8; // first
+    size_t SB = 64; // small blocks
+    char *p = mem_sbrk(HS + SB); // header and SB
+    char *h = p+8; // header
     P(p) = h;
     C(h) = SB;
     P(h) = NULL;
@@ -76,18 +93,18 @@ int mm_init(void) {
 }
 
 /* mm_malloc
- * iterate over free blocks, if one free is big enough
+ * iterate over free blocks, find best fit
  * split it if there is enough space left
- * or extend mem
+ * or extend heap
  */
 void *mm_malloc(size_t size) {
     size_t s = A(size) + HS + FS;
     char *ph = mem_heap_lo();
     char *ch = P(ph);
-    char *bp = NULL;
-    size_t best = -1;
+    char *bp = NULL; // best fit pointer
+    size_t best = -1; // big since unsigned, best fit size
     while ( ch ) {
-        size_t bs = C(ch);
+        size_t bs = C(ch); // block size
         if (bs>=s && bs<best) {
             bp = ch;
             best = bs;
@@ -99,49 +116,45 @@ void *mm_malloc(size_t size) {
         size_t bs = C(ch); // block size
         ph = ch + bs - FS;
         ph = P(ph);
-        assert(ph);
-        assert(P(ph)==ch);
-        if ( bs >= s ) { // big enough
-            size_t diff = bs - s;
-            if ( diff >= 16 + HS + FS ) { // split block
-                // allocate first half
-                char *cf = ch + s - FS;
-                C(ch) = s | 1;
-                C(cf) = s | 1;
-                // create block for second half
-                char *nmh = ch + s;
-                char *nmf = ch + bs - FS;
-                C(nmh) = diff | 1;
-                C(nmf) = diff | 1;
-                // rewiring
-                char *nh = P(ch);
-                P(ph) = nh;
-                if ( nh ) {
-                    char *nf = nh + C(nh) - FS;
-                    P(nf) = ph;
-                }
-                // will be added to linked list
-                mm_free(nmh + HS);
-            } else { // dont split
-                // allocate
-                char *cf = ch + bs - FS;
-                C(ch) = bs | 1;
-                C(cf) = bs | 1;
-                // rewiring
-                char *nh = P(ch);
-                P(ph) = nh;
-                if ( nh ) {
-                    char *nf = nh + C(nh) - FS;
-                    P(nf) = ph;
-                }
-
+        size_t diff = bs - s;
+        if ( diff >= 16 + HS + FS ) { // split block
+            // allocate first half
+            char *cf = ch + s - FS;
+            C(ch) = s | 1;
+            C(cf) = s | 1;
+            // create block for second half
+            char *nmh = ch + s;
+            char *nmf = ch + bs - FS;
+            C(nmh) = diff | 1;
+            C(nmf) = diff | 1;
+            // rewiring
+            char *nh = P(ch);
+            P(ph) = nh;
+            if ( nh ) {
+                char *nf = nh + C(nh) - FS;
+                P(nf) = ph;
             }
-            return ch + HS;
+            // will be added to linked list
+            mm_free(nmh + HS);
+        } else { // dont split
+            // allocate
+            char *cf = ch + bs - FS;
+            C(ch) = bs | 1;
+            C(cf) = bs | 1;
+            // rewiring
+            char *nh = P(ch);
+            P(ph) = nh;
+            if ( nh ) {
+                char *nf = nh + C(nh) - FS;
+                P(nf) = ph;
+            }
+
         }
-        printf("lel\n");
+        return ch + HS;
     }
     // nothing founded, go get more mem
     if (size <= 64) {
+        // small ones together
         size_t t = s * 3;
         ch = mem_sbrk(t);
         if (ch == (char *)-1) return NULL;
@@ -156,7 +169,7 @@ void *mm_malloc(size_t size) {
         C(nf) = diff | 1;
         mm_free(nh + HS);
     } else {
-        if (size < 512) {
+        if (size < 512) { // overfitting binary traces
             size = 8 * size / 7;
             s = A(size) + HS + FS;
         }
@@ -171,7 +184,8 @@ void *mm_malloc(size_t size) {
 }
 
 /* mm_free
- * rewiring and merging
+ * merge with the neighbors and rewire to skip them
+ * insert at beginning of list
  */
 void mm_free(void *vp) {
     char *ch = (char *) vp - HS;
@@ -183,7 +197,9 @@ void mm_free(void *vp) {
     if ( lo != pmf && !(C(pmf)&1)) {
         char *pmh = ch - C(pmf);
 
+        // next free
         char *nfh = P(pmh);
+        // prev free
         char *pfh = P(pmf);
 
         P(pfh) = nfh;
@@ -191,8 +207,10 @@ void mm_free(void *vp) {
         if (nfh) {
             char *nff = nfh + C(nfh) - FS;
             P(nff) = pfh;
-        }        
-        s += C(pmh);        
+        }
+
+        // update
+        s += C(pmh);
         ch = pmh;
     }
 
@@ -209,10 +227,12 @@ void mm_free(void *vp) {
         if (nfh) {
             char *nff = nfh + C(nfh) - FS;
             P(nff) = pfh;
-        }        
-        s += C(nmh);        
+        }
+        // update
+        s += C(nmh);
     }
 
+    // insert at beginning of free list
     char *cf = ch + s - FS;
     char *nh = P(lo);
     P(ch) = nh;
@@ -239,9 +259,9 @@ void *mm_realloc(void *vp, size_t size) {
     if (t >= s) return vp;
 
     char *nh = ch + t;
-    char *hi = mem_heap_hi();
+    char *hi = (char *) mem_heap_hi() + 1;
 
-    if (nh > hi) { // forget about extending for the moment
+    if (nh == hi) { // extend
         char *p = mem_sbrk(s-t);
         if (p == (char *)-1) return NULL;
         char *cf = ch + s - FS;
@@ -259,6 +279,12 @@ void *mm_realloc(void *vp, size_t size) {
     return ch;
 }
 
+/* mm_check
+ * assert invariants
+ * - no two free blocks adjacent
+ * - header and footer indicate same size
+ * - next(a) = b iff prev(b) = a
+ */
 int mm_check(void) {
     char *lo = mem_heap_lo();
     char *ph = lo;
